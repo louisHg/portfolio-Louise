3d9979e7db54de96b978db59e78004802ad1a29f
lhuyghe <lhuyghe@unis.fr>
2022-09-22 16:00:58 +0200

    page projets finit

--- assets/js/research.js
+++ assets/js/research.js
@@ -12,8 +12,6 @@ const research = [{
         citation: {
             vancouver: "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu. A Batch Normalized Inference Network Keeps the KL Vanishing Away. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
         },
-        abstract: "This is currently left empty and this can be considered as a dummy data 1",
-        absbox: "absPopup1",
     },
 
     {
@@ -26,8 +24,6 @@ const research = [{
         citation: {
             vancouver: "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre. A Call for More Rigor in Unsupervised Cross-lingual Learning. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
         },
-        abstract: "This is currently left empty and this can be considered as a dummy data 2",
-        absbox: "absPopup2",
     },
 
     {
@@ -40,8 +36,6 @@ const research = [{
         citation: {
             vancouver: "Nastaran Babanejad, Ameeta Agrawal, Aijun An and Manos Papagelis. A Comprehensive Analysis of Preprocessing for Word Representation Learning in Affective Tasks. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
         },
-        abstract: "This is currently left empty and this can be considered as a dummy data 3",
-        absbox: "absPopup3",
     },
 
     {
@@ -54,8 +48,6 @@ const research = [{
         citation: {
             vancouver: "Yong Shan, Zekang Li, Jinchao Zhang, Fandong Meng, Yang Feng, Cheng Niu and Jie Zhou. A Contextual Hierarchical Attention Network with Adaptive Objective for Dialogue State Tracking. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
         },
-        abstract: "This is currently left empty and this can be considered as a dummy data 4",
-        absbox: "absPopup4",
     },
 
     {
@@ -68,8 +60,6 @@ const research = [{
         citation: {
             vancouver: "Wang, Li and Li, Dong and Zhu, Yousong and Tian, Lu and Shan, Yi. Dual Super-Resolution Learning for Semantic Segmentation. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
         },
-        abstract: "This is currently left empty and this can be considered as a dummy data 5",
-        absbox: "absPopup5",
     },
 
     {
@@ -82,8 +72,6 @@ const research = [{
         citation: {
             vancouver: "Zhang, Kai and Van Gool, Luc and Timofte, Radu. Deep Unfolding Network for Image Super-Resolution. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
         },
-        abstract: "This is currently left empty and this can be considered as a dummy data 6",
-        absbox: "absPopup6",
     },
 
     {
@@ -96,8 +84,6 @@ const research = [{
         citation: {
             vancouver: "Liu, Yunfei and Li, Yu and You, Shaodi and Lu, Feng. Unsupervised Learning for Intrinsic Image Decomposition From a Single Image. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
         },
-        abstract: "This is currently left empty and this can be considered as a dummy data 7",
-        absbox: "absPopup7",
     },
     {
         title: "Forward and Backward Information Retention for Accurate Binary Neural Networks",
@@ -109,8 +95,6 @@ const research = [{
         citation: {
             vancouver: "Qin, Haotong and Gong, Ruihao and Liu, Xianglong and Shen, Mingzhu and Wei, Ziran and Yu, Fengwei and Song, Jingkuan. Forward and Backward Information Retention for Accurate Binary Neural Networks. IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR) 2020.",
         },
-        abstract: "This is currently left empty and this can be considered as a dummy data 8",
-        absbox: "absPopup8",
     },
 ];
 AOS.init();
@@ -125,8 +109,6 @@ const fillData = () => {
             researchYr,
             citebox,
             citation,
-            absbox,
-            abstract,
         }) =>
         (output += `
             <tr data-aos="zoom-in-left"> 
@@ -145,19 +127,15 @@ const fillData = () => {
                     </div>
                     
                     <!--CITE BUTTON-->
-                    <div class="d-flex" style="margin-right:5%;">
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${absbox}" aria-expanded="false" aria-controls="${absbox}">
-                            ABSTRACT
-                        </button>
-                
+                    <div class="d-flex" style="margin-right:5%;">                
                         <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
                             CITE
                         </button>
-                    </div>
-                    <div id="${absbox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${abstract}    
-                        </div>
+                        <a href="${image}" target="_blank">
+                            <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse">
+                                Agrandir l'image
+                            </button>
+                        </a>
                     </div>
                     <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
                         <div class="card-body">
